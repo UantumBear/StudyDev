f92e117ed75b421965c488a5757c22f6cc7085f1
yoo118 <ninkyoii@gmail.com>
2025-07-26 16:43:56 +0900

    routers -> backend/routers 로 경로 변경

---
+++ FY2025LLM/backend/routers/chat/chat001.py
@@ -0,0 +1,13 @@
+# routers/chat.py
+from fastapi import APIRouter
+from pydantic import BaseModel
+
+CHAT001 = APIRouter()
+
+class ChatRequest(BaseModel):
+    message: str
+
+@CHAT001.post("/chat")
+def chat(req: ChatRequest):
+    print(f"[CHAT] 사용자 메시지 수신: {req.message}")
+    return {"reply": f"'{req.message}'에 대한 응답입니다!"}
