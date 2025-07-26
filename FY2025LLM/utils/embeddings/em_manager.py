1f9fb27c91558c76cc344a5a1b91f473923604e5
yoo118 <ninkyoii@gmail.com>
2025-07-26 13:56:48 +0900

    임베딩 매니저

---
+++ FY2025LLM/utils/embeddings/em_manager.py
@@ -0,0 +1,15 @@
+from utils.embeddings.embedder import Embedder
+from typing import List, Dict
+
+class EmManager:
+    """
+    앱 전역에서 공유할 Embedder 객체를 래핑한 싱글톤 관리자
+    """
+    def __init__(self):
+        self.embedder = Embedder()
+
+
+""" FastAPI 주입 용도 """
+em_manager = EmManager()
+def get_em_manager() -> EmManager:
+    return em_manager
